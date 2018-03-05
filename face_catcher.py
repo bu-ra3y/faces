457e666cefb1af16b3d21b65bb9991ed7889f7cb
Will Barley <will.barley@gmail.com>
2018-03-05 05:40:37 -0500

    ADD: face_catcher: ability to provide different filters for catching images, including a timed filter

--- face_catcher.py
+++ face_catcher.py
@@ -1,16 +1,23 @@
-from pathlib import Path
-
+import abc
 from cv2 import imwrite
+from pathlib import Path
+from time import time
 
 
-class FaceCatcher():
-
+class FaceCatcher:
     OUTPUT_FOLDER = Path.home() / 'Downloads' / 'faces'
 
-    def __init__(self):
+    def __init__(self, filter=None):
         if not self.OUTPUT_FOLDER.exists():
             self.OUTPUT_FOLDER.mkdir()
+
         self.counter = 0
+
+        if filter:
+            self.filter = NoFilter()
+        else:
+            # self.filter = NoFilter()  # Default
+            self.filter = TimeFilter()  # Default
 
     @property
     def current_output_file(self):
@@ -22,14 +29,9 @@ class FaceCatcher():
             self.counter += 1
         return self.current_output_file
 
-
     def catch(self, image):
-        if self._is_a_keeper(image=image):
+        if self.filter.is_a_keeper(image=image):
             self._keep(image=image)
-    
-    @staticmethod
-    def _is_a_keeper(image) -> bool:
-        return True
 
     def _keep(self, image):
         file = self.next_output_file
@@ -38,3 +40,28 @@ class FaceCatcher():
             img=image
         )
         print(f"Saved images to {file} with return value: {retval}")
+
+
+class Filter(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def is_a_keeper(self, image):
+        """ docs """
+
+
+class NoFilter(Filter):
+    def is_a_keeper(self, image) -> bool:
+        return True
+
+
+class TimeFilter(Filter):
+    def __init__(self, seconds=10):
+        self.interval = seconds
+        self.next_time = time()  # start now
+
+    def is_a_keeper(self, image) -> bool:
+        if time() > self.next_time:  # if we have reached the allowed time
+            self.next_time = time() + self.interval  # update next allowed time
+            return True
+        else:
+            return False
